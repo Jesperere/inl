fe8e58c5d728bd902fdc3380961b3e79ded2c998
Jesperere <jesper_sylte@msn.com>
2022-03-03 13:13:24 +0100

    Initial commit

---
+++ Actions-test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Actions_test;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+        Assert.IsTrue(true);
+    }
+}
---
+++ Actions/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Actions.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
